0e925e0526ae025943b0ad7080400ebc299052fc
Drup <drupyog@zoho.com>
2016-08-18 18:08:48 +0200

    Add Tyre.all to the benchmark.

--- benchmark/benchmark_angstrom.ml
+++ benchmark/benchmark_angstrom.ml
@@ -6,26 +6,29 @@ let s = CCIO.read_all oc
 let re = Tyre.compile (Tyre.list RFC2616.request)
 let tyre s =
   match Tyre.exec re s with
-  | Result.Ok _ -> ()
+  | Result.Ok l -> assert (List.length l = 55 * 100)
+  | Result.Error _ -> failwith "oups"
+
+let re2 = Tyre.compile ~whole:false RFC2616.request
+let tyre_all s =
+  match Tyre.all re2 s with
+  | Result.Ok l -> assert (List.length l = 55 * 100)
   | Result.Error _ -> failwith "oups"
 
 let angstrom s =
   match Angstrom.(parse_only (many Angstrom_rFC2616.request)) (`String s) with
-  | Result.Ok _ -> ()
+  | Result.Ok l -> assert (List.length l = 55 * 100)
   | Result.Error _ -> failwith "oups"
-
-let l = [
-  "tyre", tyre, s ;
-  "angstrom", angstrom, s ;
-]
 
 let () =
   let open Benchmark in
   let n = 100L in
   let bench_tyre = lazy (Benchmark.latency1 n tyre s) in
+  let bench_tyre_all = lazy (Benchmark.latency1 n tyre_all s) in
   let bench_angstrom = lazy (Benchmark.latency1 n angstrom s) in
   let tree = Tree.("http" @>>> [
       "tyre" @> bench_tyre ;
+      "tyre.all" @> bench_tyre_all ;
       "angstrom" @> bench_angstrom ;
     ])
   in
